2dde1a53394b9d0b88a8582df9363f8ce0315562
thanhson7777 <thanhson11052003@gmail.com>
2025-12-30 21:11:09 +0700

    env-variable

--- src/server.js
+++ src/server.js
@@ -1,29 +1,26 @@
 /* eslint-disable no-console */
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
 const START_SERVER = () => {
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
 
   app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray())
-    process.exit(0)
     res.end('<h1>Hello World!</h1><hr>')
   })
 
-  app.listen(port, hostname, () => {
-    console.log(`3. Hello thanh son, I am running at http://${hostname}:${port}/`)
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
   })
 
   // Thực hiện các tác vụ cleanup trước khi dừng server
   exitHook(() => {
-    console.log('4. Disconnecting from MongoDB Cloud AtLas')
+    console.log('4. Disconnecting from MongoDB Cloud Atlas')
     CLOSE_DB()
-    console.log('5. Disconnected from MongoDB Cloud AtLas')
+    console.log('5. Disconnected from MongoDB Cloud Atlas')
   })
 }
 
