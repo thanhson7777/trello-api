0f8e585f8143d6d55ac7a3a6d64582ba3619ae79
thanhson7777 <thanhson11052003@gmail.com>
2026-01-01 13:56:55 +0700

    controller handing

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -3,6 +3,7 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { boardValidation } from '~/validations/boardValidation'
+import { boardController } from '~/controllers/boardController'
 
 const Router = express.Router()
 
@@ -10,6 +11,6 @@ Router.route('/')
   .get((req, res) => {
     res.status(StatusCodes.OK).json({ message: 'GET: API get list boards' })
   })
-  .post(boardValidation.createNew)
+  .post(boardValidation.createNew, boardController.createNew)
 
 export const boardRouter = Router
