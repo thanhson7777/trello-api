036271d86f760a47f713d19f742cd76ccd358e8c
thanhson7777 <thanhson11052003@gmail.com>
2026-01-06 01:14:51 +0700

    cofig-cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,2 +1,26 @@
-/**
- */
+import { WHITELIST_DOMAINS } from '~/utils/constants'
+import { env } from '~/config/environment'
+import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
+
+// Cấu hình CORD option
+export const corsOptions = {
+  origin: function (origin, callback) {
+    // Cho phép việc gọi API bằng Postman trên môi trường dev, sử dụng postman thì origin có giá trị là undefine
+    // console.log('origin: ', origin)
+    if (!origin && env.BUILD_MODE === 'dev') {
+      return callback(null, true)
+    }
+
+    // Kiểm tra xem origin có phải là domain được chấp nhận hay không
+    if (WHITELIST_DOMAINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    // Nếu domain không được chấp nhập thì sẽ trả về lỗi
+    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our cors publicy`))
+  },
+  optionsSuccessStatus: 200,
+  // Cors sẽ chấp nhận cookie từ request
+  credentials: true
+}
