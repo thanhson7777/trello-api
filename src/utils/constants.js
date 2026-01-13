baf6faa073e85b560bb0dbd842645b002b2c4e95
thanhson7777 <thanhson11052003@gmail.com>
2026-01-13 16:18:04 +0700

    update white list domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,7 @@ export const WHITELIST_DOMAINS = [
   // Không cần localhost nữa vì ở file cors đã luôn luôn cho phép chạy trên mỗi trường dev
   // 'http://localhost:5173',
   // 'http://192.168.0.197:5173'
+  'https://trello-web-six-gilt.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
