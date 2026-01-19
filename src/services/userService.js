62018d4ff32eb7a13f77e67a4e50b20e3e83b39b
thanhson7777 <thanhson11052003@gmail.com>
2026-01-19 23:14:51 +0700

    brevo send email

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,8 @@ import ApiError from '~/utils/ApiError'
 import bcryptjs from 'bcryptjs'
 import { v4 as uuidv4 } from 'uuid'
 import { pickUser } from '~/utils/formatters'
+import { WEBSITE_DOMAIN } from '~/utils/constants'
+import { BrevoProvider } from '~/providers/BrevoProvider'
 
 const createNew = async (reqBody) => {
   try {
@@ -22,10 +24,22 @@ const createNew = async (reqBody) => {
       displayName: nameFromEmail,
       verifyToken: uuidv4()
     }
+
     // Thực hiện lưu thông tin user vào trong database
     const createdUser = await userModel.createNew(newUser)
     const getNewUser = await userModel.findOneById(createdUser.insertedId)
+
     // Gửi email cho người dùng để xác thực tài khoản
+    const verifyctionLink = `${WEBSITE_DOMAIN}/account/verifycation?email=${getNewUser.email}&token=${getNewUser.verifyToken}`
+    const customSubject = 'Trello: please verify your email before using our services!'
+    const htmlContent = `
+      <h3>Here is your verify link: </h3>
+      <h3>${verifyctionLink}</h3>
+      <h3>Thanks,<br/> Trello - Thanh Sơn</h3>
+    `
+
+    // Gọi tới Provider gửi mail
+    await BrevoProvider.sendEmail(getNewUser.email, customSubject, htmlContent)
 
     // Trả dữ liệu về cho contrller
     return pickUser(getNewUser)
